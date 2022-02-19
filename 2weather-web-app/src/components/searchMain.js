e67c04705a7f3c07ebe34509cf261a630d6ea35f
Omri <omric12@gmail.com>
2022-02-19 22:17:34 +0200

    Done

--- 2weather-web-app/src/components/searchMain.js
+++ 2weather-web-app/src/components/searchMain.js
@@ -1,65 +1,65 @@
 import './style.css';
 
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 
 import WeatherDetails from './WeatherDetails';
 
 function SearchMain() {
-  const [searchTerm, setSearchTerm] = useState('haifa');
-  const [tempInfo, setTempInfo] = useState({});
+    const [searchTerm, setSearchTerm] = useState('haifa');
+    const [tempInfo, setTempInfo] = useState({});
 
-  const getWeatherInfo = async () => {
-    try {
-      let url = `http://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KRY}`;
+    const getWeatherInfo = async () => {
+        try {
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KRY}`;
 
-      let res = await fetch(url);
-      let data = await res.json();
-      const { temp, humidity, pressure } = data.main;
-      const { main: weatherType } = data.weather[0];
-      const { name } = data;
-      const { speed } = data.wind;
-      const { country, sunset } = data.sys;
+            let res = await fetch(url);
+            let data = await res.json();
+            const {temp, humidity, pressure} = data.main;
+            const {main: weatherType} = data.weather[0];
+            const {name} = data;
+            const {speed} = data.wind;
+            const {country, sunset} = data.sys;
 
-      const myNewWeatherInfo = {
-        temp,
-        humidity,
-        pressure,
-        weatherType,
-        name,
-        speed,
-        country,
-        sunset,
-      };
-      setTempInfo(myNewWeatherInfo);
-      // console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+            const myNewWeatherInfo = {
+                temp,
+                humidity,
+                pressure,
+                weatherType,
+                name,
+                speed,
+                country,
+                sunset,
+            };
+            setTempInfo(myNewWeatherInfo);
+            // console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-  useEffect(() => {
-    setTempInfo(getWeatherInfo());
-  }, []);
+    useEffect(() => {
+        setTempInfo(getWeatherInfo());
+    }, []);
 
-  return (
-    <>
-      <div className='wrap' style={{ marginTop: 2 }}>
-        <div className='search'>
-          <input
-            type='search'
-            placeholder='Search city..'
-            id='search'
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <button className='searchButton' onClick={getWeatherInfo}>
-            Search
-          </button>
-        </div>
-      </div>
-      <WeatherDetails {...tempInfo} />
-    </>
-  );
+    return (
+        <>
+            <div className='wrap' style={{marginTop: 2}}>
+                <div className='search'>
+                    <input
+                        type='search'
+                        placeholder='Search city..'
+                        id='search'
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                    <button className='searchButton' onClick={getWeatherInfo}>
+                        Search
+                    </button>
+                </div>
+            </div>
+            <WeatherDetails {...tempInfo} />
+        </>
+    );
 }
 
 export default SearchMain;
--- 3food-recipe/src/components/card/card.js
+++ 3food-recipe/src/components/card/card.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './card.style.css';
 
-function Card({ recipe }) {
-  const { label, image, url } = recipe.recipe;
-  return (
-    <div className='recipeCard'>
-      <h4 className='recipeCard__title'>{label}</h4>
-      <img
-        className='recipeCard__image'
-        src={image}
-        alt={label}
-        onClick={() => window.open(url)}
-      />
-    </div>
-  );
+function Card({recipe}) {
+    const {label, image, url} = recipe.recipe;
+    return (
+        <div className='recipeCard'>
+            <h4 className='recipeCard__title'>{label}</h4>
+            <img
+                className='recipeCard__image'
+                src={image}
+                alt={label}
+                onClick={() => window.open(url)}
+            />
+        </div>
+    );
 }
 
 export default Card;
